bb5fe8fa4ff0eb163155c56622340e8251ea1872
Guilherme Deizeppe <guideizeppe@gmail.com>
2021-10-22 00:23:50 -0300

    Adicionado Intent inform_exchange
    Adicionada a intent inform_exchange com alguns poucos dados para treino.
    Adicionado o form para validar os dados para a chamada da API de exchange.
    Adicionado custom action para validar os dados informados pelo usuário no form de exchange.

--- actions/actions.py
+++ actions/actions.py
@@ -1,8 +1,9 @@
 from typing import Text, Dict, Any, List
 
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+from rasa_sdk.types import DomainDict
 import requests
 
 
@@ -12,7 +13,7 @@ class ActionGetWeather(Action):
         return "action_get_weather"  # the actual name of the custom action which is declared in domain.yml
 
     def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker, 
+            tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         url = 'http://api.openweathermap.org/data/2.5/weather'  # API's URL
@@ -25,7 +26,7 @@ class ActionGetWeather(Action):
         complete_url = url + '?q=' + city + '&APPID=' + app_id + '&units=' + units + '&lang=' + lang + ''
 
         # Example of a complete url
-        #complete_url = 'http://api.openweathermap.org/data/2.5/weather?q=osasco&APPID=xxxxx&units=metric&lang=pt_br'
+        # complete_url = 'http://api.openweathermap.org/data/2.5/weather?q=osasco&APPID=xxxxx&units=metric&lang=pt_br'
 
         # gets the API result in a JSON format and puts into the "data" variable
         data = requests.get(complete_url).json()
@@ -45,3 +46,50 @@ class ActionGetWeather(Action):
 
         # this return the entity LOC with the city provided by the user
         return [SlotSet('LOC', city)]
+
+    class ValidateExchangeForm(FormValidationAction):
+
+        # the name of the custom action which is declared in domain.yml
+        def name(self):
+            return "validate_simple_exchange_form"
+
+        # list of supported currencies
+        @staticmethod
+        def currency_list() -> List[Text]:
+            return ['USD', 'BRL', 'EUR', 'GBP']
+
+        # validation of the slot currency1
+        def validate_currency1(
+                self,
+                slot_value: Any,
+                dispatcher: CollectingDispatcher,
+                tracker: Tracker,
+                domain: DomainDict) -> Dict[Text, Any]:
+
+            # checks if the currency1 value is in currency_list()
+            # if the result is true, the currency1 slot is set with the value provided by the user
+            # if the result is false, the currency1 slot is set to none, which will make the bot ask the user again for
+            # the correct value
+            if slot_value.upper() in self.currency_list():
+                return {"currency1": slot_value}
+            else:
+                dispatcher.utter_message('Moeda não encontrada. Por favor, digite o código da moeda corretamente.')
+                return {"currency1": None}
+
+        # validation of the slot currency2
+        def validate_currency2(
+                self,
+                slot_value: Any,
+                dispatcher: CollectingDispatcher,
+                tracker: Tracker,
+                domain: DomainDict) -> Dict[Text, Any]:
+
+            # checks if the currency2 value is in currency_list()
+            # if the result is true, the currency2 slot is set with the value provided by the user
+            # if the result is false, the currency2 slot is set to none, which will make the bot ask the user again for
+            # the correct value
+            if slot_value.upper() in self.currency_list():
+                return {"currency2": slot_value}
+            else:
+                dispatcher.utter_message('Moeda não encontrada. Por favor, digite o código da moeda corretamente.')
+                return {"currency2": None}
